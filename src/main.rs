b5aa508c3ad98c9a62ab93a5b878b97b12c6cddf
Michael Pankov <work@michaelpankov.com>
2015-11-05 13:51:11 +0300

    Use constant threads number

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,38 @@
 use std::sync::{Arc, Condvar, Mutex};
 use std::thread;
 use std::net;
-use std::io::Read;
+use std::io::{Read, Write};
 
 fn main() {
+    const THREADS_NUM: usize = 10000;
+
     let listener = Arc::new(Mutex::new(net::TcpListener::bind("127.0.0.1:10042").unwrap()));
     let counter = Arc::new((Mutex::new(0), Condvar::new()));
-    let mut threads: Vec<_> = Vec::with_capacity(10000);
-    for _ in 0..10000 {
+    let mut threads: Vec<_> = Vec::with_capacity(THREADS_NUM);
+    for i in 0..THREADS_NUM {
         let listener = listener.clone();
         let counter = counter.clone();
         let t = thread::spawn(move || {
             let (mut stream, _) = listener.lock().unwrap().accept().unwrap();
-            let mut string = String::with_capacity(8);
-            stream.read_to_string(&mut string).unwrap();
-            println!("{}", string);
+            let mut buf = Vec::new();
+            stream.read(&mut buf).unwrap();
+            let response = b"OK";
+            stream.write_all(response).unwrap();
 
             let &(ref lock, ref condvar) = &*counter;
             let mut value = lock.lock().unwrap();
+            println!("{}: {}", i, *value);
             *value += 1;
-            if *value == 10000 {
+            if *value == THREADS_NUM {
                 condvar.notify_one();
             }
+            stream.shutdown(std::net::Shutdown::Both).unwrap();
         });
         threads.push(t);
     }
     let &(ref lock, ref condvar) = &*counter;
     let mut value = lock.lock().unwrap();
-    while *value != 10000 {
+    while *value != THREADS_NUM {
         value = condvar.wait(value).unwrap();
     }
     let n: usize = *value;
